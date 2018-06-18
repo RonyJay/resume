86c3c1a2be75c202a7f06482ed614511a433d462
Rony <409169403@qq.com>
2018-06-18 16:20:31 +0800

    添加swiper轮播

---
+++ init-swiper.js
@@ -0,0 +1,24 @@
+var mySwiper = new Swiper('.swiper-container', {
+
+    loop: true,
+
+    // 如果需要分页器
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+
+    // 如果需要前进后退按钮
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    // 自动播放
+    autoplay: {
+        delay: 3000,
+        stopOnLastSlide: false,
+        disableOnInteraction: true,
+    }
+
+
+})        
