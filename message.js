3a0ea032ac13fb6dd8c5036916a5ae97710b379f
Rony <409169403@qq.com>
2018-06-24 00:21:47 +0800

    添加model

--- message.js
+++ message.js
@@ -1,18 +1,8 @@
 !function () {
   var view = document.querySelector('section.Message')
-  var controller = {
-    view: null,
-    init: function (view) {
-      this.view = view
-      this.MessagetList = view.querySelector('#MessagetList')
-      this.form = view.querySelector('form')
-      this.initAV()
-      this.loadMessages()
-      this.bindEvents()
-    },
-
-
-    initAV: function () {
+  var model = {
+    //初始化
+    init:function(){
       var APP_ID = 'tmpv7mJVJlm3bjiXnRPKaDk7-gzGzoHsz';
       var APP_KEY = 'rymopC3Ge2j5tNyF6wxnDkub';
       AV.init({
@@ -20,10 +10,38 @@
         appKey: APP_KEY
       });
     },
+    //获取数据
+    fetch:function(){
+      var query = new AV.Query('Mess');
+      return query.find()
+    },
+    //保存数据
+    save:function(name,content){
+      var Mess = AV.Object.extend('Mess');
+      var Mess = new Mess();
+      return Mess.save({
+        'name': name,
+        'content': content
+      })
+    }
+  }
+  var controller = {
+    view: null,
+    model:null,
+    init: function (view,model) {
+      this.view = view
+      this.model=model
+      this.MessagetList = view.querySelector('#MessagetList')
+      this.form = view.querySelector('form')
+      this.model.init()
+      this.loadMessages()
+      this.bindEvents()
+    },
+
+
 
     loadMessages: function () {
-      var query = new AV.Query('Mess');
-      query.find().then((Mess) => {
+      this.model.fetch().then((Mess) => {
         let array = Mess.map((item) => item.attributes)
         for (let i = 0; i < array.length; i++) {
           let li = document.createElement('li')
@@ -48,12 +66,7 @@
       let myForm = this.form
       let name = myForm.querySelector('input[name=name]').value
       let content = myForm.querySelector('input[name=content]').value
-      var Mess = AV.Object.extend('Mess');
-      var Mess = new Mess();
-      Mess.save({
-        'name': name,
-        'content': content
-      }).then(function (object) {
+      this.model.save(name,content).then(function (object) {
         let li = document.createElement('li')
         li.innerText = `${object.attributes.name} : ${object.attributes.content}`
         this.MessagetList.appendChild(li)
@@ -62,7 +75,7 @@
     },
 
   }
-  controller.init(view)
+  controller.init(view,model)
 }.call()
 
 
